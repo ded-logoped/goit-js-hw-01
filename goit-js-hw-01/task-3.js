dd2d1ca4ef7372ad71252bb269c032452166a0b5
ded-logoped <ded-logoped@ukr.net>
2024-01-21 19:56:44 +0200

    2

--- goit-js-hw-01/task-3.js
+++ goit-js-hw-01/task-3.js
@@ -1,8 +1,8 @@
-function getElementWidth(content, padding, border){
-    const contval = Number.parseFloat(content);
-    const padval = Number.parseFloat(padding);
-    const borval = Number.parseFloat(border);
-return contval+padval*2+borval*2;
+function getElementWidth(content, padding, border) {
+  const contval = Number.parseFloat(content);
+  const padval = Number.parseFloat(padding);
+  const borval = Number.parseFloat(border);
+  return contval + padval * 2 + borval * 2;
 }
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
